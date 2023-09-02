1c2f12ea7d7fcbf19b7ec2c95ae389d429488293
Yash Shetty <yashshetty@bfr.local>
2023-09-02 22:22:42 +0530

    segg

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    // baseURL: 'http://localhost:4000',
-    baseURL: "http://localhost:4000",
+    baseURL: `${import.meta.env.VITE_BASE_URL}`,
     withCredentials: true,
 })
 
